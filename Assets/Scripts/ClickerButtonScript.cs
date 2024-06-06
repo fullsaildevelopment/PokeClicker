dd4897eb49ae1c1aa94c33dd1e51f7a4edcf2eb4
John H <jharneyiiii@gmail.com>
2024-06-05 22:05:47 -0400

    added critical hits

--- Assets/Scripts/ClickerButtonScript.cs
+++ Assets/Scripts/ClickerButtonScript.cs
@@ -19,9 +19,20 @@ public class ClickerButtonScript : MonoBehaviour
     public void click()
     {
         float hp = GameManager.Instance.EnemyHP.fillAmount;
-        hp -= 0.3f;
+        if (crit())
+            hp -= 0.6f;
+        else
+            hp -= 0.3f;
+
         if (hp <= 0)
             hp = 1;
         GameManager.Instance.EnemyHP.fillAmount = hp;
+    }
+    private bool crit()
+    {
+        float rng = Random.value;
+        if (rng >= 0.9f)
+            return true;
+        return false;
     }
 }
